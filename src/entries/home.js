ce41be8355eb937fdb5bc2042229c5e38a18f4e1
Unknown <carlos15erh@gmail.com>
2019-03-19 19:27:30 -0500

    Primera creación de API
    Se añadió el logo de la empresa como extra

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Main from '../main/containers/Main.jsx';
 import login from '../../login.json';
+import api from '../api.json';
 import './home.css';
 
 const homeContainer = document.getElementById('home-container');
 
 render(<Main
   login={login.Login}
+  home={api.home}
   />, homeContainer);
