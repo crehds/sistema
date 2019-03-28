7329bbe9365fe504eddb8c2b7fe0e02790b927c3
Unknown <carlos15erh@gmail.com>
2019-03-27 21:15:42 -0500

    Se agrega page Modificar
    Solo estructura (componentes, css)

---
+++ src/pages/containers/ArrowContainer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../css/arrowcontainer.css';
+
+class ArrowContainer extends Component {
+  render() {
+    return (
+      <div className="ArrowContainer">
+        <div>Añadir</div>
+        <div>Quitar</div>
+      </div>
+    );
+  }
+}
+
+export default ArrowContainer;
--- src/pages/containers/Modificar.jsx
+++ src/pages/containers/Modificar.jsx
@@ -1,13 +1,100 @@
 import React, { Component } from 'react';
 
+import ModificarOptions from '../components/ModificarOptions.jsx';
+import SearchCotiOrder from './SearchCotiOrder.jsx';
+import DetalleMod from './DetalleMod.jsx';
+import ArrowContainer from './ArrowContainer.jsx';
+import ButtonsContainer from './ButtonContainer.jsx';
+import RelativeOptions from '../components/RelativeOptions.jsx';
+
+import LayoutDiv from '../../reusables/components/LayoutDiv.jsx';
+
+import '../css/modificar.css';
+import '../css/detalle-layout.css';
+
 class Modificar extends Component {
 
+  state = {
+    options: 'Cotización',
+    references: [],
+    referencesRelative: [],
+  };
+
+  handleForm = (event) => {
+    event.preventDefault();
+    return console.log(event);
+  };
+
+  handleReferences = (data) => (
+    this.setState({
+      referencesRelative: data,
+    })
+  );
+
+  handleOptions = (event) => (
+    this.setState({
+      options: event.target.value,
+    })
+  );
+
+  setInputRef = element => this.state.references.push(element);
+
   render() {
+    var cotizaOptions = {
+      'Condicion de pago': 'Mínimo 5 días',
+      'Validez de oferta': 'Mínimo 7 días',
+      'Tiempo de entrega': 'Máximo 30 días',
+      'Lugar de entrega': 'Lugar de entrega',
+    };
+
+    var orderOptions = {
+      'Fecha de pedido': 'El día puede variar al actual',
+      'Fecha de pago': 'Ingrese el día acordado',
+      'Términos de entrega': 'Establezca los términos',
+    };
     return (
-      <div>En desarrollo</div>
+      <form className="Modificar" onSubmit={this.handleForm}>
+        <SearchCotiOrder
+          handleOptions={this.handleOptions}
+          placeholder={this.state.options}
+        />
+        <h1 className="actual">{`Actual ${this.state.options}`}</h1>
+        <h1 className="nuevo">{`Nueva ${this.state.options}`}</h1>
+        <ModificarOptions/>
+        {this.state.options == 'Cotización' ?
+          <RelativeOptions
+            options={cotizaOptions}
+            handleReferences={this.handleReferences}
+          />
+          :
+          <RelativeOptions
+            options={orderOptions}
+            handleReferences={this.handleReferences}
+          />
+        }
+        <ModificarOptions/>
+        {this.state.options == 'Cotización' ?
+          <RelativeOptions
+          options={cotizaOptions}
+          handleReferences={this.handleReferences}
+          />
+          :
+          <RelativeOptions
+            options={orderOptions}
+            handleReferences={this.handleReferences}
+          />
+        }
+        <LayoutDiv
+          class="DetalleLayout"
+        >
+          <DetalleMod/>
+          <ArrowContainer/>
+          <DetalleMod/>
+          <ButtonsContainer/>
+        </LayoutDiv>
+      </form>
     );
   }
-
 }
 
 export default Modificar;
